11cd2f11ba9a98879271c84e533883d4c05d4dba
joymacharia <machajoy23@gmail.com>
2024-03-12 11:12:30 +0300

    created day1

---
+++ day1.py
@@ -0,0 +1,20 @@
+#Variables
+name = 'Dan'
+house_fly = 10
+
+print(name)
+
+#Data Types
+
+#primitive
+
+#String -> "" or ''
+#Number -> init, float
+#Boolean -> True, False
+#None
+
+#Non-Primitive
+#List
+#Tuple
+#Set
+#Dictionary
